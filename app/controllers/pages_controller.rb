51144b216fdaac18fef3d1fc5c4bd1e3f07273e5
Miranda Frudd <mirandafrudd@outlook.com>
2021-04-04 15:55:40 +0100

    basic finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!, only: [:home, :show]
 
   def home
   end
 
-  def about
+  def show
+    render template: "pages/#{params[:page]}"
   end
+
 end
